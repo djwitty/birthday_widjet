08b534d278c78aac1ff4e31316db7c5034720ddc
Paravozik_Misha <witty.html@gmail.com>
2019-07-28 13:44:30 +0300

    client fetch

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
 // Main JS File
-const express = require('express')
-const app = express()
-const fetch = require('node-fetch')
-
-fetch('http://test.anromsocial.com/api/birthdays?dateFrom=01.01&dateTo=01.02', {
-        method: 'get',
-        body:    params,
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res => res.json())
-    .then(json => console.log(json));
-
-app.get('/', function (req, res) {
-  res.send(data)
-})
- 
-app.listen(3000)
--- public/src/main.js
+++ public/src/main.js
@@ -1,1 +1,14 @@
-// import Vue.js
+var dataWrap = document.getElementById('data');
+
+fetch('http://test.anromsocial.com/api/birthdays?dateFrom=01.01&dateTo=01.02', {
+    method: 'get',
+    headers: { 'Content-Type': 'application/json' }
+})
+.then(res => res.json())
+//.then(json => console.log(json))
+.then(json => {
+  for(var u in json) {
+    dataWrap.innerHTML = u
+  }
+});
+
